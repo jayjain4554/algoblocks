373dfcf8792aafce4bfb419f38b6dd8f3e2912df
jayjain4554 <jayjainff@gmail.com>
2025-04-18 12:36:58 +0530

    done

--- backend/app.py
+++ backend/app.py
@@ -122,3 +122,8 @@ def home():
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(host="0.0.0.0", port=7777)
--- backend/database.py
+++ backend/database.py
@@ -1,13 +1,21 @@
-from flask_sqlalchemy import SQLAlchemy
-import json
+# from flask_sqlalchemy import SQLAlchemy
+# import json
 
+# db = SQLAlchemy()
+
+# class Strategy(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String(80), unique=True, nullable=False)
+#     config = db.Column(db.Text, nullable=False)
+
+#     def __init__(self, name, config):
+#         self.name = name
+#         self.config = json.dumps(config)
+
+from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Strategy(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
-    config = db.Column(db.Text, nullable=False)
-
-    def __init__(self, name, config):
-        self.name = name
-        self.config = json.dumps(config)
+    name = db.Column(db.String(100))
+    config = db.Column(db.JSON)
